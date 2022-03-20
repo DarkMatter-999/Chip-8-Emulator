ab0aa171d2a247a9a18397e2cd766449d8a33019
DarkMatter-999 <darkmatter999official@gmail.com>
2022-03-20 19:04:46 +0530

    Added basic SDL2 frontend

--- c8_core/src/lib.rs
+++ c8_core/src/lib.rs
@@ -447,6 +447,21 @@ impl Emu {
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
         }
     }
+
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        self.keys[idx] = pressed;
+    }
+
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+
+        self.ram[start..end].copy_from_slice(data);
+    }
 }
 
 
